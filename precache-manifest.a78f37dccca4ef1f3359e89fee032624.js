f6d9deb4c7845bd8706e272d8a4c02e0c776bffe
anurav <anuravmodak1@gmail.com>
2022-06-06 13:30:43 +0530

    Updates

---
+++ precache-manifest.a78f37dccca4ef1f3359e89fee032624.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13d4f63065ed283379281c2934e7129",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "219a21e714cf9534ed6b",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "9dc1b47a6fc4476ff486",
+    "url": "/box-office/static/js/2.45e9879c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.45e9879c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "219a21e714cf9534ed6b",
+    "url": "/box-office/static/js/main.7e821bae.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
